35bac17eed00f2d51cd878af02bb12a66dbc6964
Juan David Navarro Jimenez <juan.navarro@mail.escuelaing.edu.co>
2019-09-14 10:01:52 -0500

    Parte 2

---
+++ servlet/target/servlet-1/WEB-INF/classes/edu/eci/cvds/servlet/model/Todo.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.eci.cvds.servlet.model;
+
+/**
+ *
+ * @author 2105057
+ */
+public class Todo {
+    
+    //atributos
+    private int userId;
+    private int id;
+    private String title;
+    private boolean completed;
+    
+    public Todo (){
+        
+    }
+    
+    public int getUserId(){
+        return userId;
+    }
+    
+    public int getId(){
+        return id;
+    }
+    
+    public String getTitle(){
+        return title;
+    }
+    
+    public boolean getCompleted(){
+        return completed;
+    }
+    
+    public void setUserId(int userId){
+        this.userId = userId;
+    }
+    
+    public void setId(int id){
+        this.id = id;
+    }
+    
+    public void setTitle(String title){
+        this.title = title;
+    }
+    
+    public void setCompleted(boolean completed){
+        this.completed = completed;
+    }
+    
+}
