3e1aaeea5d54833af020644f6ad22ca2b48dc95d
Juan David Navarro Jimenez <juan.navarro@mail.escuelaing.edu.co>
2019-09-18 20:47:47 -0500

    README y documentacion

--- servlet/target/classes/edu/eci/cvds/servlet/model/Todo.java
+++ servlet/target/classes/edu/eci/cvds/servlet/model/Todo.java
@@ -1,13 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package edu.eci.cvds.servlet.model;
 
 /**
- *
- * @author 2105057
+ * Clase Todo que es la representacion de un JSON en un objeto java. 
  */
 public class Todo {
     
@@ -21,34 +15,66 @@ public class Todo {
         
     }
     
+    /**
+     * Metodo que permite consultar el id del usuario.
+     * @return  Retorna el userID.
+     */
     public int getUserId(){
         return userId;
     }
     
+    /**
+     * Metodo que permite consultar el id.
+     * @return  Retorna el id.
+     */
     public int getId(){
         return id;
     }
     
+    /**
+     * Metodo que permite consultar el titulo.
+     * @return  Retorna el titulo.
+     */
     public String getTitle(){
         return title;
     }
     
+    /**
+     * Metodo que permite consultar el estado.
+     * @return  Retorna el estado.
+     */
     public boolean getCompleted(){
         return completed;
     }
-    
+
+    /**
+     * Metodo que permite la modificacion del userId.
+     * @param userId    Es el nuevo userId.
+     */
     public void setUserId(int userId){
         this.userId = userId;
     }
     
+    /**
+     * Metodo que permite la modificacion del id.
+     * @param id    Es el nuevo id.
+     */
     public void setId(int id){
         this.id = id;
     }
     
+    /**
+     * Metodo que permite la modificacion del titulo
+     * @param title     Es el nuevo titulo.
+     */
     public void setTitle(String title){
         this.title = title;
     }
     
+    /**
+     * Metodo que permite la modificacion del estado
+     * @param completed     Es el nuevo estado.
+     */
     public void setCompleted(boolean completed){
         this.completed = completed;
     }
